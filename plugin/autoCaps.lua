189a8e6babbc7ae81f3b200018a6ebddc794e041
Ben <ben76020567@gmail.com>
2024-12-06 11:22:12 +0800

    refac

--- plugin/autoCaps.lua
+++ plugin/autoCaps.lua
@@ -7,24 +7,37 @@ else
 	distDir = root .. "/dist"
 end
 
-if vim.fn.isdirectory(distDir) ~= 1 then
-	if isWindows then
-		vim.fn.mkdir(root .. "\\dist")
-		local dir = root .. "\\src\\windows"
-		os.execute("gcc -o " .. root .. "\\dist\\capsLockOff.exe " .. dir .. "\\capsLockOff.c")
-	else
-		vim.fn.mkdir(root .. "/dist")
-		local dir = root .. "/src/linux"
-		os.execute("gcc -o " .. root .. "/dist/capsLockOff " .. dir .. "/capsLockOff.c -lX11 -lXtst")
-	end
+local function build()
+  if isWindows then
+    vim.fn.mkdir(root .. "\\dist")
+    local dir = root .. "\\src\\windows"
+    os.execute("gcc -o " .. root .. "\\dist\\capsLockOff.exe " .. dir .. "\\capsLockOff.c")
+  else
+    vim.fn.mkdir(root .. "/dist")
+    local dir = root .. "/src/linux"
+    os.execute("gcc -o " .. root .. "/dist/capsLockOff " .. dir .. "/capsLockOff.c -lX11 -lXtst")
+  end
 end
 
-if isWindows then
-	vim.api.nvim_create_user_command("CapsLockOff", function()
-		vim.fn.system(root .. "\\dist\\capsLockOff.exe")
-	end, { nargs = 0 })
-else
-	vim.api.nvim_create_user_command("CapsLockOff", function()
-		vim.fn.system(root .. "/dist/capsLockOff")
-	end, { nargs = 0 })
+if vim.fn.isdirectory(distDir) ~= 1 then
+  build()
 end
+
+vim.api.nvim_create_user_command("CapsLockOff", function()
+  if isWindows then
+    vim.fn.system(root .. "\\dist\\capsLockOff.exe")
+  else
+    vim.fn.system(root .. "/dist/capsLockOff")
+  end
+end, { nargs = 0 })
+
+vim.api.nvim_create_user_command("CapsLockRebuild", function()
+  build()
+end, { nargs = 0 })
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = "*",
+  callback = function()
+    vim.cmd("CapsLockOff")
+  end,
+})
